5961e91af1e20a6efca23458c0ad176846b76b87
ParSiAnToTo <wushuchj@gmail.com>
2025-07-14 14:11:27 +0900

    init : create CRA and setup typescript, tailwindcss setting

---
+++ shooking/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-2xl font-bold text-blue-600 p-4">
+      Tailwind is working! 🎉
+    </div>
+  );
+}
+
+export default App;
